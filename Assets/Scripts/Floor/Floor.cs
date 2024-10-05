ad371980138c9675dde6b2934ad8f66aadf89654
Kellyn Mendez <kpm.mendez@gmail.com>
2024-10-05 13:23:28 -0500

    floor shake

--- Assets/Scripts/Floor/Floor.cs
+++ Assets/Scripts/Floor/Floor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 
 public class Floor : MonoBehaviour
 {
@@ -11,6 +12,10 @@ public class Floor : MonoBehaviour
     [SerializeField] float fallTime = 1.5f;
     [SerializeField] float fallDistance = 20f;
     [SerializeField] float waitToRespawn = 3f;
+
+    [Header("Shake Settings")]
+    [SerializeField] float shakeSpeed = 100f;
+    [SerializeField] float shakeAmount = 0.02f;
 
     [Header("Miscellaneous")]
     [SerializeField] GameObject artToDiasble;
@@ -30,11 +35,17 @@ public class Floor : MonoBehaviour
         if (_fallTriggered == true)
         {
             _timerToFall -= Time.deltaTime;
+            // Shake platform
+            this.transform.position = new Vector3(
+                _startPos.x + (Mathf.Sin(_timerToFall * shakeSpeed) * shakeAmount),
+                _startPos.y + (Mathf.Sin(_timerToFall * shakeSpeed) * shakeAmount),
+                _startPos.z + (Mathf.Sin(_timerToFall * shakeSpeed) * shakeAmount));
         }
 
         // Once timer is finished, platform falls
         if (_fallTriggered == true && _timerToFall <= 0f)
         {
+            this.transform.position = _startPos;
             // Making platform fall
             Vector3 toPos = new Vector3(
                 _startPos.x, 
